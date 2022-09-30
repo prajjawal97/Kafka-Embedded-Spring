f11d74ef1cea05fc50f5b221d9fd7a383c9c5e64
prajjawal97 <prajjawalkansal.it20@gmail.com>
2022-09-30 22:52:43 +0530

    added data

---
+++ src/main/java/KafkaEmbeddedSpring/KafkaEmbeddedSpring/KafkaEmbeddedSpringApplication.java
@@ -0,0 +1,13 @@
+package KafkaEmbeddedSpring.KafkaEmbeddedSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEmbeddedSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaEmbeddedSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KafkaEmbeddedSpring/KafkaEmbeddedSpring/KafkaEmbeddedSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package KafkaEmbeddedSpring.KafkaEmbeddedSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEmbeddedSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
